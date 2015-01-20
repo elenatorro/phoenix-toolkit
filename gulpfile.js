00be8ed2e716a337de63b5071b692dab2bc6b59a
Elena Torro <elenatorro@gmail.com>
2015-01-20 16:22:03 +0100

    task for component compilations (both js and css)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var path = require('path');
+var sass = require('gulp-sass');
+var argv = require('yargs').argv;
+var inject = require('gulp-inject');
 
 /************* Paths *************/
 /* from */
@@ -27,22 +30,21 @@ gulp.task('lint', function() {
 
 /* TODO */
 
-//task to erase components
-
 //task to compile script component
-gulp.task('compile_script_component', function(name) {
-  return gulp.src('config/scripts/components/' + name + '/*.js')
-  .pipe(concat(name + '.js'))
-  .pipe(gulp.dest('assets/scripts/components/' + name))
-});
+gulp.task('compile_script_components', function() {
+  return gulp.src('config/scripts/components/' + argv.comp + '/*.js')
+  .pipe(concat('script.js'))
+  .pipe(gulp.dest('assets/scripts/components/' + argv.comp));
+})
 
-//task to compile style component
-gulp.task('compile_style_component', function() {
-  return gulp.src('config/styles/components/' + process.argv[0] + '/*.scss')
-  .pipe(sass())
-  .pipe(concat(process.argv[0] + '.css'))
-  .pipe(gulp.dest('assets/styles/components/' + process.argv[0]))
-});
+  //task to compile style component
+  gulp.task('compile_style_components', function(name) {
+    return gulp.src('config/styles/components/' + argv.comp + '/*.scss')
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('assets/styles/components/' + argv.comp));
+  })
+
 
 //task to compile general sass to css
 gulp.task('style', function() {
@@ -61,5 +63,13 @@ gulp.task('scripts', function() {
   .pipe(uglify())
   .pipe(gulp.dest('public/scripts'));
 });
+
+//html injections
+gulp.task('yields', function() {
+  return gulp.src('views/' + argv.comp + '.html')
+  .pipe(inject(gulp.src('views/components/header.html', {read: false}), {name: 'header'}))
+  .pipe(gulp.dest('./'));
+})
+
 
 gulp.task('default');
